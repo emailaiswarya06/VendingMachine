0a750b073dc990d22d69dd1fd65d7c6f55cef73f
emailaiswarya06 <emailaiswarya06@gmail.com>
2021-11-29 18:22:30 +0530

    commit 3 -  Refactor to avoid code duplication

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,26 +1,58 @@
+
+
 import java.util.Scanner;
-import java.util.SortedMap;
 
 public class VendingMachine {
    static double totalAmount;
 
+    static int itemValue1 = 10;
+    static int itemValue2 = 5;
+    static int itemValue3 = 20;
+    static int itemValue4 = 6;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char flag;
+
+        int itemValue=0;
+
         System.out.println("WELCOME !! Please choose from below items :");
         System.out.println("0:  Cancel");
-        System.out.println("1:  Kitkat\tRs 10 ");
-        System.out.println("2:  Gems\tRs 5 ");
-        System.out.println("3:  Soda\tRs 20 ");
-        System.out.println("4:  Lays\tRs 6 ");
+        System.out.println("1:  Kitkat\t Rs "+itemValue1);
+        System.out.println("2:  Gems\t Rs "+itemValue2);
+        System.out.println("3:  Soda\tRs "+itemValue3);
+        System.out.println("4:  Lays\tRs "+itemValue4);
+
+
         do {
 
             System.out.println("Kindly enter the item ");
-
             int itemId = sc.nextInt();
+
+            switch (itemId){
+
+                case 0:
+                    break;
+
+                case 1:
+                    itemValue = itemValue1;
+                    break;
+                case 2:
+                    itemValue = itemValue2;
+                    break;
+                case 3:
+                    itemValue = itemValue3;
+                    break;
+                case 4:
+                    itemValue = itemValue4;
+                    break;
+
+            }
+
+
             double inputAmount = 0;
             //valid input item id check
-            if (itemId < 0 || itemId > 5) {
+            if (itemId < 0 || itemId > 4) {
                 System.out.println("Please enter Valid input item");
 
             } else {
@@ -28,8 +60,7 @@ public class VendingMachine {
                     System.out.println("Enter Amount");
                     inputAmount = sc.nextDouble();
                 }
-                payment(itemId, inputAmount);
-
+                payment(itemId, itemValue,inputAmount);
 
             }
             System.out.println("Do you still want more items. Press 'Y' to Continue and 'N' to exit");
@@ -39,54 +70,38 @@ public class VendingMachine {
         System.out.println("Total Amount in vending machine = "+totalAmount);
     }
 
-    static void payment(int itemId,double inputAmount ){
+    static void payment(int itemId,int itemValue,double inputAmount ){
                 double balanceAmount;
-            switch (itemId){
 
-                case 0:
-                    System.out.println("Thank you! Please collect your money back");
-                    break;
-                case 1:
-                    if (inputAmount>=10){
-                        balanceAmount = inputAmount -10;
-                        totalAmount = totalAmount+balanceAmount;
-                        System.out.println("Balance :"+balanceAmount);
-                    }
-                    else {
-                        System.out.println("Insufficient Balance");
-                    }
-                    break;
-                case 2:
-                    if (inputAmount>=5){
-                        balanceAmount = inputAmount -5;
-                        totalAmount = totalAmount+balanceAmount;
-                        System.out.println("Balance :"+balanceAmount);
-                    }
-                    else {
-                        System.out.println("Insufficient Balance");
-                    }
-                    break;
-                case 3:
-                    if (inputAmount>=20){
-                        balanceAmount = inputAmount -20;
-                        totalAmount = totalAmount+balanceAmount;
-                        System.out.println("Balance :"+balanceAmount);
-                    }
-                    else {
-                        System.out.println("Insufficient Balance");
-                    }
-                    break;
-                case 4:
-                    if (inputAmount>=6){
-                        balanceAmount = inputAmount -6;
-                        totalAmount = totalAmount+balanceAmount;
-                        System.out.println("Balance :"+balanceAmount);
-                    }
-                    else {
-                        System.out.println("Insufficient Balance");
-                    }
-                    break;
-            }
+                switch (itemId){
+
+                    case 0:
+                        System.out.println("Thank you! Please collect your money back");
+                        break;
+
+                    default:
+                        if (itemId<5){
+
+
+                            if (inputAmount>=itemValue){
+                                balanceAmount = inputAmount -itemValue;
+                                totalAmount = totalAmount+balanceAmount;
+                                System.out.println("Balance :"+balanceAmount);
+                            }
+                            else {
+                                System.out.println("Insufficient Balance");
+                            }
+
+                        }
+                        else {
+
+                            System.out.println("Please enter a valid input.");
+                        }
+
+                        break;
+
+                }
+
     }
 
 }
